04b5d28f2eafe9a14b66cca09345f357d8b793ed
AdovaX <58963746+AdovaX@users.noreply.github.com>
2020-11-17 14:16:08 +0530

    Repair Design added

--- src/app/Dk/repair/repair.component.ts
+++ src/app/Dk/repair/repair.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+interface Food {
+  value: string;
+  viewValue: string;
+}
 @Component({
   selector: 'app-repair',
   templateUrl: './repair.component.html',
   styleUrls: ['./repair.component.css']
 })
 export class RepairComponent implements OnInit {
+isLinear = true;
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
 
-  constructor() { }
+    foods: Food[] = [
+    {value: 'steak-0', viewValue: 'Steak'},
+    {value: 'pizza-1', viewValue: 'Pizza'},
+    {value: 'tacos-2', viewValue: 'Tacos'}
+  ];
+  
+  constructor(private _formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
   }
-
 }
