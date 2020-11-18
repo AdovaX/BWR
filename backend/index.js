76b7c842e718c3974d70ee1b0313230d978fe166
AdovaX <58963746+AdovaX@users.noreply.github.com>
2020-11-18 22:01:29 +0530

    Service & Model set

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-
+ 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
